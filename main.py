f29800f142563d3c7046483c80dced3ab8b3a3cb
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2021-09-27 14:33:06 +0100

    Added an enemy ship class

--- main.py
+++ main.py
@@ -62,6 +62,22 @@ class Player(Ship):
         self.max_health = health
 
 
+class Enemy(Ship):
+    COLOR_MAP = {
+        "red": (RED_SPACE_SHIP, RED_LASER),
+        "green": (GREEN_SPACE_SHIP, GREEN_LASER),
+        "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
+    }
+
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
+
+
 def main():
     run = True
     fps = 60
