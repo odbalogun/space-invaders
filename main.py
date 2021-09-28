a1652589aff893b8975e24989fa666d1e18916d0
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2021-09-28 14:22:08 +0100

    Added a Laser class

--- main.py
+++ main.py
@@ -29,6 +29,26 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return height >= self.y >= 0
+
+    def collision(self, obj):
+        return collide(obj, self)
+
+
 class Ship:
     def __init__(self, x, y, health=100):
         self.x = x
@@ -76,6 +96,10 @@ class Enemy(Ship):
 
     def move(self, vel):
         self.y += vel
+
+
+def collide(obj1, obj2):
+    pass
 
 
 def main():
