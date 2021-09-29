52648913b314345d63ffeac755c07b7a827031c4
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2021-09-30 00:55:41 +0100

    Added methods to detect collision

--- main.py
+++ main.py
@@ -99,7 +99,9 @@ class Enemy(Ship):
 
 
 def collide(obj1, obj2):
-    pass
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) is not None
 
 
 def main():
